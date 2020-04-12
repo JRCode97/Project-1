6e10d8ad5f4ce695bb63c749b7d15423c6648614
Jesse Rivera <35208196+codiene@users.noreply.github.com>
2020-04-12 15:59:02 -0400

    added dao tests and implemented service

--- ExpenseReimbursementSystem/src/main/java/dev/rivera/daos/ReimbursementDAOdb.java
+++ ExpenseReimbursementSystem/src/main/java/dev/rivera/daos/ReimbursementDAOdb.java
@@ -3,16 +3,39 @@ package dev.rivera.daos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import dev.rivera.backend.ConnectionUtils;
 import dev.rivera.entities.Employee;
 import dev.rivera.entities.Reimbursement;
+import dev.rivera.daos.*;
 
-
-public class ReimbursementDAOdb implements ReimbusementDAO {
-
+public class ReimbursementDAOdb implements ReimbursementDAO {
+	public Reimbursement createReimbursement(Reimbursement r) {
+		
+		try(Connection con = ConnectionUtils.createConnection()) {
+			
+			
+			String Query = "INSERT into REIMBURSEMENT VALUES (0,?,?,?,?)";
+			PreparedStatement post = con.prepareStatement(Query,Statement.RETURN_GENERATED_KEYS);
+			post.setInt(1, r.getAmount());
+			post.setString(2, r.getDescription());
+			post.setString(3, r.getStatus());
+			post.setInt(4, r.getRequesterId());
+			post.execute();
+			ResultSet rs = post.getGeneratedKeys();
+			rs.next();
+			int key = rs.getInt("ID");
+			r.setrId(key);
+			return r;
+			
+	}catch(Exception e){
+		System.out.println(e);
+	}
+		return r;
+	}
 	public List<Reimbursement> getAllReimbursements() {
 		List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
 		try(Connection con = ConnectionUtils.createConnection()) {
@@ -24,7 +47,7 @@ public class ReimbursementDAOdb implements ReimbusementDAO {
 			
 			while(rs.next()) {
 				System.out.println("found reimbursement");
-				Reimbursement currentReimbursement = new Reimbursement(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getInt(4),rs.getString(5));
+				Reimbursement currentReimbursement = new Reimbursement(rs.getInt("ID"),rs.getInt("AMOUNT"),rs.getString("DESCRIPTION"),rs.getInt("REQUESTERID"),rs.getString("STATUS"));
 				System.out.println(currentReimbursement);
 				reimbursements.add(currentReimbursement);
 			}if(!rs.next())
@@ -47,7 +70,7 @@ try(Connection con = ConnectionUtils.createConnection()) {
 			
 			if(rs.next()) {
 				System.out.println("found reimbursement");
-				 reimbursement = new Reimbursement(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getInt(4),rs.getString(5));
+				 reimbursement = new Reimbursement(rs.getInt("ID"),rs.getInt("AMOUNT"),rs.getString("DESCRIPTION"),rs.getInt("REQUESTERID"),rs.getString("STATUS"));
 				System.out.println(reimbursement);
 				return reimbursement;
 			}if(!rs.next())
@@ -58,8 +81,10 @@ try(Connection con = ConnectionUtils.createConnection()) {
 	return reimbursement;
 	}
 
-	public Reimbursement getReimbursementByEmployee(Employee e) {
+	public List<Reimbursement> getReimbursementByEmployee(Employee e) {
 		Reimbursement reimbursement = null;
+		List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
+
 		try(Connection con = ConnectionUtils.createConnection()) {
 					
 					
@@ -68,17 +93,18 @@ try(Connection con = ConnectionUtils.createConnection()) {
 					recieve.setInt(1, e.geteId());
 					ResultSet rs = recieve.executeQuery();
 					
-					if(rs.next()) {
+					while(rs.next()) {
 						System.out.println("found reimbursement");
-						 reimbursement = new Reimbursement(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getInt(4),rs.getString(5));
+						 reimbursement = new Reimbursement(rs.getInt("ID"),rs.getInt("AMOUNT"),rs.getString("DESCRIPTION"),rs.getInt("REQUESTERID"),rs.getString("STATUS"));
 						System.out.println(reimbursement);
-						return reimbursement;
+						reimbursements.add(reimbursement);
+						return reimbursements;
 					}if(!rs.next())
 						System.out.println("no reimbursements found");
 			}catch(Exception ex){
 				System.out.println(ex);
 			}
-			return reimbursement;
+			return reimbursements;
 	}
 
 	public Reimbursement UpdateReimbursement(Reimbursement r) {
@@ -90,6 +116,7 @@ try(Connection con = ConnectionUtils.createConnection()) {
 			ps.setString(3, r.getDescription());
 			ps.setString(4, r.getStatus());
 			ps.setInt(5, r.getRequesterId());
+			ps.setInt(6, r.getrId());
 			ps.executeUpdate();
 			return r;
 		}catch(Exception ex) {
